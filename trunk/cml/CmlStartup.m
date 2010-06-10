dbbd28412b35ac316b9c77a23fa684dee40db223
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2010-06-10 21:25:05 +0000

    essage

--- trunk/cml/CmlStartup.m
+++ trunk/cml/CmlStartup.m
@@ -18,19 +18,19 @@ cml_home = pwd;
 % this is the location of the mex directory for this architecture
 switch computer
     case 'PCWIN'  % MS Windows on x86
-        addpath( strcat( cml_home, '\mex\mexpcwin');
+        addpath( strcat( cml_home, '\mex\pcwin') );
     case 'GLNX86' % Linux on x86
-        addpath( strcat( cml_home, '/mex/mexglnx86');
+        addpath( strcat( cml_home, '/mex/glnx86') );
     case 'MACI'   % Apple Mac OS X on x86         
-        addpath( strcat( cml_home, '/mex/mexmaci');
+        addpath( strcat( cml_home, '/mex/maci') );
     case 'PCWIN64' % Microsoft Windows on x64      
-        addpath( strcat( cml_home, '\mex/mexpcwin64');
+        addpath( strcat( cml_home, '\mex\pcwin64') );
     case 'GLNXA64'  % Linux on x86_64      
-        addpath( strcat( cml_home, '/mex/mexglnxa64');
-    case 'SOL64'    % Sun Solaris on SPARC          
-        addpath( strcat( cml_home, '/mex/mexsol64');
+        addpath( strcat( cml_home, '/mex/glnxa64') );
+    case 'SOL64'    % Sun Solaris on SPARC           
+        addpath( strcat( cml_home, '/mex/sol64') );
     case 'MACI64'   % Apple Mac OS X on x86_64   
-        addpath( strcat( cml_home, '/mex/mexmaci64');
+        addpath( strcat( cml_home, '/mex/maci64') );
 end
 
 if ispc
