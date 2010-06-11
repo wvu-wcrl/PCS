a4e03a19dab7538a458a222d9e89ed7eb91077e1
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2010-06-11 17:46:29 +0000

    Corrected path in CmlStartup.m

--- cml/CmlStartup.m
+++ cml/CmlStartup.m
@@ -2,7 +2,7 @@
 %
 % Initializes the Coded Modulation Library
 %
-% Last updated June 9, 2010
+% Last updated June 11, 2010
 
 % determine the version of matlab version
 version_text = version;
@@ -14,24 +14,6 @@ end
 
 % determine the home directory
 cml_home = pwd;
-
-% this is the location of the mex directory for this architecture
-switch computer
-    case 'PCWIN'  % MS Windows on x86
-        addpath( strcat( cml_home, '\mex\pcwin') );
-    case 'GLNX86' % Linux on x86
-        addpath( strcat( cml_home, '/mex/glnx86') );
-    case 'MACI'   % Apple Mac OS X on x86         
-        addpath( strcat( cml_home, '/mex/maci') );
-    case 'PCWIN64' % Microsoft Windows on x64      
-        addpath( strcat( cml_home, '\mex\pcwin64') );
-    case 'GLNXA64'  % Linux on x86_64      
-        addpath( strcat( cml_home, '/mex/glnxa64') );
-    case 'SOL64'    % Sun Solaris on SPARC           
-        addpath( strcat( cml_home, '/mex/sol64') );
-    case 'MACI64'   % Apple Mac OS X on x86_64   
-        addpath( strcat( cml_home, '/mex/maci64') );
-end
 
 if ispc
     % setup the path
@@ -73,6 +55,24 @@ else
 
     % save the home directory
     save_directory = strcat( cml_home, '/scenarios/CmlHome.mat' );
+end
+
+% this is the location of the mex directory for this architecture
+switch computer
+    case 'PCWIN'  % MS Windows on x86
+        addpath( strcat( cml_home, '\mex\pcwin') );
+    case 'GLNX86' % Linux on x86
+        addpath( strcat( cml_home, '/mex/glnx86') );
+    case 'MACI'   % Apple Mac OS X on x86         
+        addpath( strcat( cml_home, '/mex/maci') );
+    case 'PCWIN64' % Microsoft Windows on x64      
+        addpath( strcat( cml_home, '\mex\pcwin64') );
+    case 'GLNXA64'  % Linux on x86_64      
+        addpath( strcat( cml_home, '/mex/glnxa64') );
+    case 'SOL64'    % Sun Solaris on SPARC           
+        addpath( strcat( cml_home, '/mex/sol64') );
+    case 'MACI64'   % Apple Mac OS X on x86_64   
+        addpath( strcat( cml_home, '/mex/maci64') );
 end
 
 save( save_directory, save_flag, 'cml_home' );
