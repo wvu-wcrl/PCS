ea77ed86d6ada42e51c8ec48ec51fce3634a14f7
terry.ferrett@gmail.com <terry.ferrett@gmail.com@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-02-28 20:20:42 +0000

    Completed worker starting method.

--- trunk/ee561/srv/tests/start_worker_test.m
+++ trunk/ee561/srv/tests/start_worker_test.m
@@ -5,9 +5,15 @@
 % 2/27/2011
 % Terry Ferrett
 
-function cwc_obj = cwc_constructor_test(cml_home)
+function cwc_obj = start_worker_test(cml_home)
 
-cmlRoot = cml_home;
+cd ..;
+cd ..;
+cmlRoot = pwd;
+cd srv/
+cd tests/
 
 % Create cluster worker object
 cwc_obj = cwc(cmlRoot, 'test.cfg', 'stub_worker');
+
+cwc_obj.wSta('node01');
