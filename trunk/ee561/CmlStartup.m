39dd219734cadb9326b7fff612e31afb0ec59d0b
terry.ferrett@gmail.com <terry.ferrett@gmail.com@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-02-28 00:34:56 +0000

    Added initial version of cluster worker controller.

--- trunk/ee561/CmlStartup.m
+++ trunk/ee561/CmlStartup.m
@@ -2,18 +2,25 @@
 %
 % Initializes CML 2 by setting the path
 %
-% Last updated July 15, 2010
+% Last updated February 27, 2011 by Terry
+% Latest change:
+% Added worker controller and utility code directories to the path.
+
 
 % determine the home directory
 cml_home = pwd;
 
+% set the path
 if ispc
-    % setup the path
-    addpath( strcat( cml_home, '\mat') );
+    addpath( strcat( cml_home, '\mat' ) );
+    addpath( strcat( cml_home, '\util' ) );
+    addpath( strcat( cml_home, '\srv' ) );
 else
-    % setup the path
-    addpath( strcat( cml_home, '/mat') );
+    addpath( strcat( cml_home, '/mat' ) );
+    addpath( strcat( cml_home, '/util' ) );
+    addpath( strcat( cml_home, '/srv' ) );
 end
+
 
 % this is the location of the mex directory for this architecture
 switch computer
@@ -21,14 +28,14 @@ switch computer
         addpath( strcat( cml_home, '\mex\pcwin') );
     case 'GLNX86' % Linux on x86
         addpath( strcat( cml_home, '/mex/glnx86') );
-    case 'MACI'   % Apple Mac OS X on x86         
+    case 'MACI'   % Apple Mac OS X on x86
         addpath( strcat( cml_home, '/mex/maci') );
-    case 'PCWIN64' % Microsoft Windows on x64      
+    case 'PCWIN64' % Microsoft Windows on x64
         addpath( strcat( cml_home, '\mex\pcwin64') );
-    case 'GLNXA64'  % Linux on x86_64      
+    case 'GLNXA64'  % Linux on x86_64
         addpath( strcat( cml_home, '/mex/glnxa64') );
-    case 'SOL64'    % Sun Solaris on SPARC           
+    case 'SOL64'    % Sun Solaris on SPARC
         addpath( strcat( cml_home, '/mex/sol64') );
-    case 'MACI64'   % Apple Mac OS X on x86_64   
+    case 'MACI64'   % Apple Mac OS X on x86_64
         addpath( strcat( cml_home, '/mex/maci64') );
 end
