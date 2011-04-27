54cee1a8dd8058a29d3260bf4b3069ff4163b8e0
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-04-27 14:18:49 +0000

    Corrections made to JobManager

--- ahfh/tests/TestWorker.m
+++ ahfh/tests/TestWorker.m
@@ -19,12 +19,12 @@ OmegaFileName = 'Omega_N20_M50_r30.mat';
 save( OmegaFileName, 'Omega' )
 
 % create the Param structure
-Param.m_i = 3*ones(1,50);
-Param.m = m;
-Param.Gamma = 10*log(1:10);
-Param.Beta = [0.1 1 10];
-Param.p = [1 0.1 0.01 0.001];
-Param.OmegaFileName = OmegaFileName;
+JobParam.m_i = 3*ones(1,50);
+JobParam.m = m;
+JobParam.Gamma = 10*log(1:10);
+JobParam.Beta = [0.1 1 10];
+JobParam.p = [1 0.1 0.01 0.001];
+JobParam.OmegaFileName = OmegaFileName;
 
 % save the structure in the "input" directory
-save( 'Test1.mat', 'Param' );
+save( 'Test1.mat', 'JobParam' );
