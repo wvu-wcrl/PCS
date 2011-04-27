b1dca8c4dd73c19014271b8b392f42aa8ef448d3
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-04-27 02:57:31 +0000

    stub worker accepts cmlPath as second argument

--- ahfh/srv/wrk/stub_worker.m
+++ ahfh/srv/wrk/stub_worker.m
@@ -5,14 +5,17 @@
 % Terry Ferrett
 % Version 1
 % 2/27/2011
-function stub_worker(n)
+function stub_worker(n,cmlRoot)
 
-TimeToLive = 600;
+TimeToLive = 60;
 t1=tic;
 
+InFile  =  [cmlRoot '/input/i' int2str(n)];  
+OutFile = [cmlRoot '/output/o' int2str(n)];  
+
 while(toc(t1) < TimeToLive )
-    filename = ['dummy' int2str(n)];  
-    save( filename, 't1' );
+    save( InFile, 't1' );
+    save( OutFile, 't1' );
     pause(5);
 end
     
