45d91f0d532e701a451519e621daf2cf839c71ff
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-04-28 17:33:48 +0000

    correction to ClusterStatus

--- trunk/ahfh/util/ClusterStatus.m
+++ trunk/ahfh/util/ClusterStatus.m
@@ -5,7 +5,7 @@ function NumberWorkers = ClusterStatus( hosts, worker_name )
 NumberWorkers = zeros(1,length(hosts));
 
 for i=1:length( hosts )
-    command_string = ['ssh ' cwc_obj.nodes{i} ' ps aux|grep -i ' worker_name];
+    command_string = ['ssh ' hosts{i} ' ps aux|grep -i ' worker_name];
     [~,zz] = system( command_string );
     hits = findstr( zz, 'outage_worker' );
     NumberWorkers( i ) = length( hits );
