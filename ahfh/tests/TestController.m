fdfdbbc5e677abf884ee10c582dd04a5628bfada
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-04-28 03:51:17 +0000

    Removed SNR = 0 from example in TestController

--- ahfh/tests/TestController.m
+++ ahfh/tests/TestController.m
@@ -8,17 +8,31 @@ ahfhRoot = cml_home;
 % set the parameters
 m = 3;
 M = 50;
+m_i = 3;  % should be constant for all interferers (for now)
 
-% The file where the network coefficients "Omega" are stored.
-OmegaFileName = 'Omega_N20_M50_r30.mat';
+% Create an object
+load( [ahfhRoot '/tables/Omega_N20_M50_r30.mat'] )
+
+% The object needs to be called this
+b = OutageNakagami( Omega, m, m_i );
+
+% The name of where an example OutageNakagami object is stored
+% Previously we called this OmegaFileName
+% We use an OutageNakagami object to store m, m_i, and Omega 
+NetworkFileName = 'Network1.mat';
+
+% save b
+save( [ahfhRoot '/tables/' NetworkFileName ]. 'b' );
+
+SNRdB = [10 20];
 
 % Create the Param structure
 JobParam.m_i = 3*ones(1,M);
 JobParam.m = m;
-JobParam.Gamma = 10*log(1:10);
+JobParam.Gamma = 10.^(SNRdB/10);
 JobParam.Beta = [0.1 1 10];
 JobParam.p = [1 0.1 0.01 0.001];
-JobParam.OmegaFileName = OmegaFileName;
+JobParam.NetworkFileName = NetworkFileName;
 
 % Test multiple jobs
 NumberJobs = 3;
