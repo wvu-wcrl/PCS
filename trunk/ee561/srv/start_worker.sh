4c786af31f0d5b815240fd0c7241f351b454742f
Matthew.C.Valenti <Matthew.C.Valenti@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-05-01 18:55:51 +0000

    Initial Version of ee561_worker

--- trunk/ee561/srv/start_worker.sh
+++ trunk/ee561/srv/start_worker.sh
@@ -10,9 +10,10 @@ hostname=$1
 worker_path=$2
 worker_exe=$3
 worker_id=$4
+cml_path=$5
 
 # Connect to the node and start the worker as a daemon.
-ssh $hostname "export MATLABPATH=$worker_path; nohup matlab -r $worker_exe\($worker_id\) > /dev/null 2>&1 &"
+ssh $hostname "export MATLABPATH=$worker_path; nohup matlab -r $worker_exe\($worker_id,\'$cml_path\'\) > /dev/null 2>&1 &"
 
 # Get the process ID.
-ssh $hostname "ps -ef" |grep -i "matlab -r $worker_exe($worker_id)" | awk '{ print $2 }'
+ssh $hostname "ps -ef" |grep -i "matlab -r $worker_exe($worker_id," | awk '{ print $2 }'
