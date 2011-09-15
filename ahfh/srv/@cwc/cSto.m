7f60e8634e97c0d23a2435c5ae46eb82a8754dcd
terry.ferrett <terry.ferrett@gmail.com>
2011-09-15 16:41:32 +0000

    added feature to stop cluster workers by worker script name

--- ahfh/srv/@cwc/cSto.m
+++ ahfh/srv/@cwc/cSto.m
@@ -1,15 +1,44 @@
 % cSto.m
 %
-% Stop all cluster workers.
+% Stop cluster workers.
 %
-% Version 1
-% 2/28/2011
+% If called with no arguments
+% Version 2
+% 9/15/2011
 % Terry Ferrett
 
-function cSto(obj)
-% Loop over all active workers and end worker processes.
-num_workers = length(obj.workers);
-for k = 1:num_workers,
-    wSto(obj, obj.workers(1).wrkCnt);
-end
-end
+
+
+function cSto(obj,varargin)
+
+if nargin == 0,   % kill all workers unconditionally
+    % Loop over all active workers and end worker processes.
+    num_workers = length(obj.workers);
+    for k = 1:num_workers,
+        wSto(obj, obj.workers(1).wrkCnt);
+    end
+    
+else if nargin == 1,
+        
+        % Loop over all active workers and stop workers running a particular worker script.
+        num_workers = length(obj.workers);
+        ws = varargin{1};
+        
+        % gather the IDs of the workers running the script 'ws'
+        for k = 1:num_workers,
+            if obj.workers(k).ws == ws,
+                wrkid(k) = obj.worker(k).wrkCnt;
+            end
+        end
+        
+        % stop the workers running script 'ws'
+        for k = 1:length(wrkid),
+            wSto(obj, wrkid(k));
+        end
+        
+        
+    else
+        error('Too many arguments to function cSto()');
+    end
+    
+end    
