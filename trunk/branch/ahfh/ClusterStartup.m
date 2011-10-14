4c68e582863f3d2aa6d5bac5d300092824c34dc6
terry.ferrett@gmail.com <terry.ferrett@gmail.com@2e022cc8-daf8-11dd-86d7-074df07e0730>
2011-10-14 19:23:16 +0000

    moved worker controller startup scripts to live in individual project directories

--- trunk/branch/ahfh/ClusterStartup.m
+++ trunk/branch/ahfh/ClusterStartup.m
@@ -8,35 +8,51 @@
 %    Added worker controller and utility code directories to the path.
 %    On April 20, 2011 MCV changed the name of the file from CmlStartup to ClusterStartup
 
+   function [proj_dir root_dir] = ClusterStartup(resume)
+
+
 % determine the home directory
-cml_home = pwd;
+   proj_dir = pwd; cd ..; root_dir = pwd; cd(proj_dir);
+
+
 
 % set the path
 if ispc
-    addpath( strcat( cml_home, '\mat' ) );
-    addpath( strcat( cml_home, '\util' ) );
-    addpath( strcat( cml_home, '\srv' ) );
-else
-    addpath( strcat( cml_home, '/mat' ) );
-    addpath( strcat( cml_home, '/util' ) );
-    addpath( strcat( cml_home, '/srv' ) );
+    addpath( strcat( proj_dir, '\mat' ) );
+addpath(proj_dir, '\state');
+addpath( strcat( root_dir, '\srv' ) );
+addpath( strcat( root_dir, '\srv','\util' ) );
+ else
+   addpath(strcat(proj_dir, '/state'));
+    addpath( strcat( proj_dir, '/mat' ) );
+addpath( strcat( root_dir, '/srv' ) );
+addpath( strcat( root_dir, '/srv','/util' ) );
 end
 
 
+
+if resume == 1,
+
 % Load the cluster worker controller state, if it exists.
-cwcRelativePath = strcat('srv',...
-    '/', 'state');
-cwcFullPath = strcat(cml_home,...
-    '/', cwcRelativePath);
+%cwcRelativePath = strcat('srv',...
+%    '/', 'state');
+%cwcFullPath = strcat(cml_home,...
+%    '/', cwcRelativePath);
+
+
+
 try
     fprintf('Attempting to load cluster controller state from file\n %s\n\n', cwcRelativePath);
-    cd(cwcFullPath)
+%    cd(cwcFullPath)
     load('cwc_state.mat');
-    cd(cml_home);
+%    cd(cml_home);
 fprintf('State loaded.\n')
 catch
     fprintf('State file does not exist.  Aborting load.\n');
-    cd(cml_home);
+%    cd(cml_home);
 end
 
+
+
+end
 % can add paths to c-mex, if necessary
