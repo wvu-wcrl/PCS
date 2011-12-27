01bdf54574e4c154745a1085c92fd14ad3d12b73
terry.ferrett <terry.ferrett@gmail.com>
2011-12-27 04:22:36 +0000

    moved ctc and cwc into directory wvu_pcs/, short for "WVU Parallel Computing Suite".  added cluster worker controller for generic worker

---
+++ wvu_pcs/ctc/src/cs.m
@@ -0,0 +1,44 @@
+% cs.m
+%
+% Initializes EE561 project (cluster code) by setting the path
+%
+% Last updated February 27, 2011 by Terry
+%
+% Latest changes:
+%    Added worker controller and utility code directories to the path.
+%    On April 20, 2011 MCV changed the name of the file from CmlStartup to ClusterStartup
+
+   function [prjroot srvroot] = cs(resume)
+
+
+% determine the home directory
+   prjroot = pwd; 
+%cd ..; cd srv/; srvroot = pwd; cd(prjroot);
+
+
+
+% set the path
+if ispc
+    addpath( strcat( prjroot, '\tc' ) );
+addpath(prjroot, '\util');
+ else
+   addpath(strcat(prjroot, '/tc'));
+    addpath( strcat( prjroot, '/util' ) );
+end
+
+
+
+if resume == 1,
+
+try
+    fprintf('Attempting to load cluster controller state from file\n %s\n\n', cwcRelativePath);
+    load('cwc_state.mat');
+fprintf('State loaded.\n')
+catch
+    fprintf('State file does not exist.  Aborting load.\n');
+end
+
+
+
+end
+
