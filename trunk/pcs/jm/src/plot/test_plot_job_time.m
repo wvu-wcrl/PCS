c3295ec80710f7324a6fad1e44dd62cd7777c76c
terry.ferrett@gmail.com <terry.ferrett@gmail.com@2e022cc8-daf8-11dd-86d7-074df07e0730>
2012-01-20 17:19:41 +0000

    added plotting scripts to codedmod job manager

---
+++ trunk/pcs/jm/src/plot/test_plot_job_time.m
@@ -0,0 +1,9 @@
+data = [1 2 5 6;...
+             2 4 5 7;...
+             3 5 7 8];
+         
+         time = [0 2 4 5];
+         
+         nodelist = {'node01','node02'};
+         
+         plot_job_time(data,time,nodelist);
