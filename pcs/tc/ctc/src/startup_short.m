de67e093c0bbe132c08d7352afb96e273f8a77ae
terry.ferrett <terry.ferrett@gmail.com>
2012-05-15 17:37:45 +0000

    fixed bugs in pcs startup procedure

--- pcs/tc/ctc/src/startup_short.m
+++ pcs/tc/ctc/src/startup_short.m
@@ -1,27 +1,44 @@
 % startup script for task controller
+%
+%%%inputs
+%
+% cfg_file
+%  configuration file specifying task controller parameters
+%
+%
+% startup_type
+%  'startup' - clear global and user running queues
+%  'resume' - leave queues in existing state and start controllers
+%%%%%%%%%% 
 
-rootdir = pwd;
+
+
+
+function ctcobj = tc_startup(cfg_file, startup_type)
+
+
+
+rootdir = pwd;              % make the current directory the root
 addpath(rootdir);
+
 
 
 cd ..; cd ..; cd ..;
 cd util/; addpath(pwd);  % add file parser code to path
 
 
+
+
 cd(rootdir);
+cd ..; cd ..; cd cfg/; cfg_file = strcat(pwd,'/', cfg_file);     % current config file
 
-
-cd ..; cd ..; cd cfg/; cfg_file = strcat(pwd,'/','pcs_short_224.cfg'); % current config file
-
-
-
-
-%addpath(strcat(rootdir, '/tc'))
 
        
 
 cd(rootdir);
+ctcobj = ctc(cfg_file, startup_type);     % create task controller object
 
 
 
-ctcobj = ctc(cfg_file);
+
+end
