8ef74b5cd8c3416645a156417fd3ddcdeb93358c
terry.ferrett@gmail.com <terry.ferrett@gmail.com@2e022cc8-daf8-11dd-86d7-074df07e0730>
2012-09-19 03:38:40 +0000

    first in a series of updates to CmlJobManager to support exit simulation

--- trunk/pcs/jm/cml/src/@CmlJobManager/UpdateJobState.m
+++ trunk/pcs/jm/cml/src/@CmlJobManager/UpdateJobState.m
@@ -3,27 +3,44 @@ function JobState = UpdateJobState(obj, JobStateIn, TaskState)
 JobState = JobStateIn;      % Update the Global JobState.
 
 % Convert the randomly-permuted SNR points of the task back to the order in which the JobState is saved.
-TaskState = DepermuteSnrPoints( TaskState );
+TaskState = DepermuteSnrPoints( TaskState, JobState );
 
-JobState.trials      = JobState.trials      + TaskState.trials;    % update trials
-if ~strcmpi( JobState.sim_type, 'bloutage' )
-    JobState = UpdateOutageStats( JobState, TaskState );
+
+switch JobState.sim_type,
     
-    if ~strcmpi( JobState.sim_type, 'coded' )   % If uncoded, update symbol error rate, too.
-        [ JobState TaskState ] = UpdateBERStats( JobState, TaskState );
-    end
+    case{'coded', 'uncoded'},   %%% this logic needs to be refactored - Terry 9/2012
+        
+        JobState.trials      = JobState.trials      + TaskState.trials;    % update trials
+        if ~strcmpi( JobState.sim_type, 'bloutage' )
+            JobState = UpdateBERFERStats( JobState, TaskState );
+            
+            if ~strcmpi( JobState.sim_type, 'coded' )   % If uncoded, update symbol error rate, too.
+                [ JobState TaskState ] = UpdateSERStats( JobState, TaskState );
+            end
+        end
+        
+    case{'exit'},
+        JobState = UpdateExitStats(JobState, TaskState);
+        
 end
 end
 
 
-function TaskState = DepermuteSnrPoints ( TaskState )
+function TaskState = DepermuteSnrPoints ( TaskState, JobState )
 TaskState.trials(:,TaskState.RandPos) = TaskState.trials;
-TaskState.bit_errors(:,TaskState.RandPos) = TaskState.bit_errors;
-TaskState.frame_errors(:,TaskState.RandPos) = TaskState.frame_errors;
+
+switch JobState.sim_type,
+    case{'coded', 'uncoded'},
+        TaskState.bit_errors(:,TaskState.RandPos) = TaskState.bit_errors;
+        TaskState.frame_errors(:,TaskState.RandPos) = TaskState.frame_errors;
+    case{'exit'},
+        TaskState.exit_state.IA_det_sum(:,TaskState.RandPos) = TaskState.exit_state.IA_det_sum(:,TaskState.RandPos);
+        TaskState.exit_state.IE_det_sum(:,TaskState.RandPos) = TaskState.exit_state.IE_det_sum(:,TaskState.RandPos);
+end
 end
 
 
-function JobState = UpdateOutageStats( JobState, TaskState )
+function JobState = UpdateBERFERStats( JobState, TaskState )
 JobState.bit_errors   = JobState.bit_errors   + TaskState.bit_errors;
 JobState.frame_errors = JobState.frame_errors + TaskState.frame_errors;
 
@@ -32,7 +49,7 @@ JobState.FER = JobState.frame_errors ./ JobState.trials;
 end
 
 
-function [ JobState TaskState ] = UpdateBERStats( JobState, TaskState )
+function [ JobState TaskState ] = UpdateSERStats( JobState, TaskState )
 if( JobState.mod_order > 2 )
     % Update symbol error counter.
     TaskState.symbol_errors(:,TaskState.RandPos) = TaskState.symbol_errors;
@@ -42,4 +59,13 @@ else
     JobState.symbol_errors = JobState.bit_errors;
     JobState.SER = JobState.BER;
 end
+end
+
+
+function JobState = UpdateExitStats(JobState, TaskState)
+JobState.trials = JobState.trials      + TaskState.trials;    % update trials
+JobState.exit_state.IA_det_sum = JobState.exit_state.IA_det_sum +...
+    TaskState.exit_state.IA_det_sum;
+JobState.exit_state.IE_det_sum = JobState.exit_state.IE_det_sum +...
+    TaskState.exit_state.IE_det_sum;
 end
