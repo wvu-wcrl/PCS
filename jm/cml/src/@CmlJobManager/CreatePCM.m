4460439947d1304d7e41b3e36709ee77db602dd5
mohammad.fanaei <mohammad.fanaei@2e022cc8-daf8-11dd-86d7-074df07e0730>
2013-01-29 21:59:53 +0000

    A bug in CreatePCM was fixed.

--- jm/cml/src/@CmlJobManager/CreatePCM.m
+++ jm/cml/src/@CmlJobManager/CreatePCM.m
@@ -1,31 +1,26 @@
 % CreatePCM converts parity check matrices uploaded by web users
-%   to the format expected by CML.
+% to the format expected by CML.
 %
 % The calling syntax is:
-%     CreatePCM( obj, CurrentUser, pcm, cml_home )
+%     [SuccessFlag, ErrMsg, H_rows, H_cols] = CreatePCM( obj, CurrentUser, pcm )
 %
 %     Inputs
-%       obj         CmlJobManager object
-%       CurrentUser Current user selected by CmlJobManager
-%       pcm         parity check matrix argument loaded from
-%                     sim_param.parity_check_matrix
-%       data_path   path to user data directory on cluster
+%       obj         CmlJobManager object.
+%       CurrentUser Current user selected by CmlJobManager.
+%       pcm         parity check matrix argument loaded from sim_param.parity_check_matrix.
 %
 %     Outputs
-%       SuccessFlag      0 if no error, 1 if error
-%       ErrMsg           Empty if no error, string if error
+%       SuccessFlag      0 if no error, 1 if error.
+%       ErrMsg           Empty if no error, string if error.
+%       H_rows
+%       H_cols
 %
-%
-%     Copyright (C) 2013, Terry R. Ferrett, Mohammad Fanaei and Matthew C. Valenti
-%
-%     Last updated on 1/25/2013
+%     Copyright (C) 2013, Terry R. Ferrett, Mohammad Fanaei, and Matthew C. Valenti.
+%     Last updated on 1/25/2013.
 
+function [SuccessFlag ErrMsg H_rows H_cols] = CreatePCM( obj, CurrentUser, pcm )
 
-%%% Terry: return H_rows and H_cols
-function [SuccessFlag ErrMsg H_rows H_cols] = ...
-    CreatePCM( obj, CurrentUser, pcm )
-
-SuccessFlag = 0;  % assume successful operation
+SuccessFlag = 0;  % Assume successful operation.
 
 hmat_type = get_hmat_type( pcm );
 
@@ -34,22 +29,21 @@ cml_home = CurrentUser.CodeRoot;
 switch hmat_type
     case 'pchk'
         pcm_prefix = pcm(1:end-5);
-        [ H_rows H_cols ErrMsg ] = cnv_pchk_hr_hc( obj, pcm, cml_home );
+        [ H_rows, H_cols, ErrMsg ] = cnv_pchk_hr_hc( obj, pcm, CurrentUser, cml_home );
         
         if ~isempty(ErrMsg)
             SuccessFlag = 1;
             return;
         end
         
-        %[ErrMsg] = save_hrows_hcols( obj, CurrentUser, pcm_prefix, H_rows, H_cols);
+        % [ErrMsg] = save_hrows_hcols( obj, CurrentUser, pcm_prefix, H_rows, H_cols );
+        
     case 'alist'
         pcm_prefix = pcm(1:end-6);
-        [JobInDir, JobRunningDir, JobOutDir, TempDir JobDataDir] =...
-            obj.SetPaths(CurrentUser.JobQueueRoot);
-        [H_rows H_cols] = CmlAlistToHrowsHcols( JobDataDir, pcm_prefix );
-        ErrMsg = '';  % add error checking later
-        % return H_rows, H_cols, create code_param_long
-        %  [ErrMsg] = save_hrows_hcols( obj, CurrentUser, pcm_prefix, H_rows, H_cols);
+        [JobInDir, JobRunningDir, JobOutDir, TempDir JobDataDir] = obj.SetPaths(CurrentUser.JobQueueRoot);
+        [H_rows, H_cols] = CmlAlistToHrowsHcols( JobDataDir, pcm_prefix );
+        ErrMsg = '';  % add error checking later.
+        % [ErrMsg] = save_hrows_hcols( obj, CurrentUser, pcm_prefix, H_rows, H_cols );
     case 'mat'
         %
     case 'cml_dvbs2'
@@ -65,9 +59,7 @@ if ~isempty(ErrMsg)
     return;
 end
 
-
 end
-
 
 
 function hmat_type = get_hmat_type( pcm )
@@ -90,25 +82,24 @@ end
 end
 
 
-function [ H_rows H_cols ErrMsg ] = cnv_pchk_hr_hc( obj, pcm, cml_home )
+function [ H_rows H_cols ErrMsg ] = cnv_pchk_hr_hc( obj, pcm, CurrentUser, cml_home )
 
-[LdpcCodeGenP] = crp_codegen_cl( cml_home );
+LdpcCodeGenP = crp_codegen_cl( cml_home );
 
 tmp_path = obj.JobManagerParam.TempJMDir;
 
-[JobInDir, JobRunningDir, JobOutDir, TempDir JobDataDir] =...
-    obj.SetPaths(CurrentUser.JobQueueRoot);
+[JobInDir, JobRunningDir, JobOutDir, TempDir JobDataDir] = obj.SetPaths(CurrentUser.JobQueueRoot);
 
 [ErrMsg] = cnv_pchk_alist_cl(LdpcCodeGenP, JobDataDir, tmp_path, pcm);
 
 pcm_prefix = pcm(1:end-5);
 
-[H_rows H_cols] = CmlAlistToHrowsHcols( tmp_path, pcm_prefix );
+[H_rows, H_cols] = CmlAlistToHrowsHcols( tmp_path, pcm_prefix );
 
 end
 
 
-function [LdpcCodeGenP] = crp_codegen_cl( cml_home )
+function LdpcCodeGenP = crp_codegen_cl( cml_home )
 
 p1 = cml_home;
 p2 = 'module';
@@ -121,23 +112,23 @@ LdpcCodeGenP = [p1 filesep p2 filesep p3 filesep p4 filesep p5];
 end
 
 
-function [ErrMsg] = cnv_pchk_alist_cl(LdpcCodeGenP, data_path, tmp_path, pchk_file)
+function [ErrMsg] = cnv_pchk_alist_cl(LdpcCodeGenP, DataPath, TmpPath, pchk_file)
 
-% create alist creation command string
-sp = [' '];
-p1 = ['pchk-to-alist'];   % command name
-p2 = [data_path filesep pchk_file];  % temporary filename
-p3 = [tmp_path filesep pchk_file(1:end-5) '.alist'];
+% Create alist creation command string.
+sp = ' ';
+p1 = 'pchk-to-alist'; % command name.
+p2 = [DataPath filesep pchk_file]; % Temporary filename.
+p3 = [TmpPath filesep pchk_file(1:end-5) '.alist'];
 
-if ~ exist([LdpcCodeGenP filesep p1], 'file');
+if ~exist([LdpcCodeGenP filesep p1], 'file')
     ErrMsg1 = 'pchk-to-alist executable file does not exist.\n';
     ErrMsg2 = 'Please compile the LDPC code generation functions.';
     ErrMsg = [ErrMsg1 ErrMsg2];
 else
-    ErrMsg = [''];
+    ErrMsg = '';
 end
 
-% invoke external command to generate alist file
+% Invoke external command to generate alist file.
 cmd = [ps sp LdpcCodeGenP filesep p1 sp p2 sp p3];
 system(cmd);
 end
@@ -150,20 +141,19 @@ end
 %
 % save( PcmMatTmp, 'H_rows', 'H_cols');
 %
-% [JobInDir, JobRunningDir, JobOutDir, TempDir] =...
-%     obj.SetPaths(CurrentUser.JobQueueRoot);
+% [JobInDir, JobRunningDir, JobOutDir, TempDir] = obj.SetPaths(CurrentUser.JobQueueRoot);
 %
 % PcmMatDataP = [ JobDataDir filesep PcmMat ];
 %
-% % error check
-% SuccessFlag = MoveFile(obj, PcmMatTmpP, PcmMatDataP,...
-%     SuccessMsg, ErrorMsg);
+% % Error check.
+% SuccessFlag = MoveFile(obj, PcmMatTmpP, PcmMatDataP, SuccessMsg, ErrorMsg);
 % if SuccessFlag ~= 0,
-%     ErrMsg = ['Error saving %s.', PcmMatDataP];
+%     ErrMsg = ['Error saving DATA file %s.', PcmMatDataP];
 % else
 %     ErrMsg = [''];
 % end
 % end
+
 
 %     Function CmlCreateHmatCluster is part of the Iterative Solutions Coded Modulation
 %     Library (ISCML).
