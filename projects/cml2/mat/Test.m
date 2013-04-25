bbe3984c040ebe830cb517e8dbc3d8a1ed4f82c3
mohammad.fanaei <mohammad.fanaei@2e022cc8-daf8-11dd-86d7-074df07e0730>
2013-04-25 21:03:58 +0000

    A new class called BinaryUncodedModulation was added to the CML2 project. This class inherits from the UncodedModulation class and its inputs are bits rather than M-ary symbols.

--- projects/cml2/mat/Test.m
+++ projects/cml2/mat/Test.m
@@ -1,15 +1,15 @@
 %% Testing BinaryUncodedModulation class:
-clear all
+clear classes
 MM = CreateModulation([-1 1]);
 C = AWGN( MM, 20 );
-D = BinaryUncodedModulation(2, 4);
+D = BinaryUncodedModulation(2, [], 1, 4);
 
 M = D.Encode([0 0 1 1])
 SymbolLikelihood = C.ChannelUse(M)
 EstBits = D.Decode(SymbolLikelihood)
 
 %% Testing the speed of randp vs gDiscrPdfRnd functions for small blocks:
-clear all
+clear classes
 K = 2000; N = 10;
 T_RandP = zeros(K,1);
 T_gDiscrPdfRnd = zeros(K,1);
@@ -21,7 +21,7 @@ T_RandP_Avg = mean(T_RandP)
 T_gDiscrPdfRnd_Avg = mean(T_gDiscrPdfRnd)
 
 %% Testing the speed of randp vs MATLAB's built-in rand functions for large blocks:
-clear all
+clear classes
 K = 20; N = 100000;
 T_RandP = zeros(K,1);
 T_Rand = zeros(K,1);
@@ -33,7 +33,7 @@ T_RandP_Avg = mean(T_RandP)
 T_Rand_Avg = mean(T_Rand)
 
 %% Testing UncodedModulation class for a small block:
-clear all
+clear classes
 clc
 SymbolProb = [0.2 0.2 0.3 0.3];
 SignalSet = [-1 -1  1  1
@@ -54,7 +54,7 @@ SymbolLikelihood = C.ChannelUse(M);
 % [NumBitError, NumSymbolError] = D.ErrorCount(SymbolLikelihood)
 
 %% Testing UncodedModulation class for a large block and finding the error rate:
-clear all
+clear classes
 clc
 SymbolProb = [0.2 0.2 0.3 0.3];
 SignalSet = [-1 -1  1  1
@@ -65,15 +65,15 @@ BlockLength = 10000;
 MM = CreateModulation(SignalSet, SymbolProb);
 C = AWGN( MM, 10^(SNRdB/10) );
 D = UncodedModulation(4, [], [], BlockLength, 10*SymbolProb);
-hist(D.DataSymbols, [1:length(D.SymbolProb)]);
 
 M = D.Encode();
+hist(D.DataSymbols, [1:length(D.SymbolProb)]);
 SymbolLikelihood = C.ChannelUse(M);
 [EstBits, NumBitError, NumSymbolError, EstSymbols]  = D.Decode(SymbolLikelihood);
 % [NumBitError, NumSymbolError] = D.ErrorCount(SymbolLikelihood)
 
 %% Testing BinaryUncodedModulation class for a small block:
-clear all
+clear classes
 clc
 SymbolProb = [0.2 0.2 0.3 0.3];
 SignalSet = [-1 -1  1  1
@@ -83,16 +83,19 @@ BlockLength = 10;
 
 MM = CreateModulation(SignalSet, SymbolProb);
 C = AWGN( MM, 10^(SNRdB/10) );
-D = BinaryUncodedModulation(4, BlockLength);
+
+DemodType = 0;
+ZeroRandFlag = 1;
+D = BinaryUncodedModulation(4, DemodType, ZeroRandFlag, BlockLength);
 
 M = D.Encode()
 DataBits = D.DataBits
 SymbolLikelihood = C.ChannelUse(M);
-[EstBits, EstSymbols] = D.Decode(SymbolLikelihood)
+[EstBits, NumBitError, NumSymbolError, EstSymbols] = D.Decode(SymbolLikelihood)
 [NumBitError, NumSymbolError] = D.ErrorCount(SymbolLikelihood)
 
 %% Testing BinaryUncodedModulation class for a large block and finding the error rate:
-clear all
+clear classes
 clc
 SymbolProb = [0.2 0.2 0.3 0.3];
 SignalSet = [-1 -1  1  1
@@ -102,15 +105,17 @@ BlockLength = 1000;
 
 MM = CreateModulation(SignalSet, SymbolProb);
 C = AWGN( MM, 10^(SNRdB/10) );
-D = BinaryUncodedModulation(4, BlockLength);
+DemodType = 0;
+ZeroRandFlag = 1;
+D = BinaryUncodedModulation(4, DemodType, ZeroRandFlag, BlockLength);
 
 M = D.Encode();
 SymbolLikelihood = C.ChannelUse(M);
-[EstBits, EstSymbols] = D.Decode(SymbolLikelihood);
+[EstBits, NumBitError, NumSymbolError, EstSymbols] = D.Decode(SymbolLikelihood);
 [NumBitError, NumSymbolError] = D.ErrorCount(SymbolLikelihood)
 
 %% Testing BinaryUncodedModulation class with QPSK class for a large block and finding the error rate:
-clear all
+clear classes
 clc
 SymbolProb = [0.2 0.2 0.3 0.3];
 SNRdB = 10;
@@ -118,15 +123,17 @@ BlockLength = 10000;
 
 MM = QPSK(SymbolProb);
 C = AWGN( MM, 10^(SNRdB/10) );
-D = BinaryUncodedModulation(4, BlockLength);
+DemodType = 0;
+ZeroRandFlag = 1;
+D = BinaryUncodedModulation(4, DemodType, ZeroRandFlag, BlockLength);
 
 M = D.Encode();
 SymbolLikelihood = C.ChannelUse(M);
-[EstBits, EstSymbols] = D.Decode(SymbolLikelihood);
+[EstBits, NumBitError, NumSymbolError, EstSymbols] = D.Decode(SymbolLikelihood);
 [NumBitError, NumSymbolError] = D.ErrorCount(SymbolLikelihood)
 
 %% Testing LinkSimulation class:
-clear all
+clear classes
 SymbolProb = [0.2 0.2 0.3 0.3];
 SignalSet = [-1 -1  1  1
     -1  1 -1  1];
@@ -147,34 +154,34 @@ SimParam = struct(...
     'MaxRunTime', 300, ...     % Maximum simulation time in Seconds.
     'CheckPeriod', 500, ...    % Checking time in number of Trials.
     'MaxBitErrors', 5000*ones(size([0:0.5:10])), ...
-    'MaxSymErrors', 5000*ones(size([0:0.5:10])), ...
+    'MaxFrameErrors', 5000*ones(size([0:0.5:10])), ...
     'minBER', 1e-5, ...
-    'minSER', 1e-5 );
+    'minFER', 1e-5 );
 
 Link = LinkSimulation(SimParam);
-Link.SingleSimulate();
+SimState = Link.SingleSimulate()
 
 %% Comparing union bound on SER with exact average SER and simulated SER for 16-QAM.
-clear all
+clear classes
 NoSignals = 16;
-SymbolProb = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1];
+SymbolProb = ones(1,NoSignals);
 %  SymbolProb = [1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4];
-MM = QAM(NoSignals, SymbolProb);
+MM = QAM(NoSignals, [], SymbolProb);
 SNRdB = -5:0.5:25;
-UpperSymbolBoundValue = LinkSimulation.UnionBoundSymbol( MM.SignalSet, 10.^(SNRdB/10), SymbolProb/sum(SymbolProb) )
+% UpperSymbolBoundValue = LinkSimulation.UnionBoundSymbol( MM.SignalSet, 10.^(SNRdB/10), SymbolProb/sum(SymbolProb) );
 
 %%% Exact average symbol error probability for QAM.
 % Distance is the same matrix generated in UnionBoundSymbol method of AWGN class.
-Distance = AWGN.GetSignalDistance(MM.SignalSet);
-DistanceA = Distance;
-DistanceA(Distance == 0) = Inf;
-D = min( min(DistanceA) );
-QAM_ExactPs = zeros(1, length(SNRdB));
-for m = 1:length(SNRdB)
-    Q1 = 0.5 * ( 1 - erf( sqrt(10.^(SNRdB(m)/10))*D / 2 ) );
-    QAM_ExactPs(m) = 4*(1 - 1/sqrt(NoSignals)) * Q1 * (1 - (1 - 1/sqrt(NoSignals)) * Q1);
-end
-QAM_ExactPs
+% Distance = AWGN.GetSignalDistance(MM.SignalSet);
+% DistanceA = Distance;
+% DistanceA(Distance == 0) = Inf;
+% D = min( min(DistanceA) );
+% QAM_ExactPs = zeros(1, length(SNRdB));
+% for m = 1:length(SNRdB)
+%     Q1 = 0.5 * ( 1 - erf( sqrt(10.^(SNRdB(m)/10))*D / 2 ) );
+%     QAM_ExactPs(m) = 4*(1 - 1/sqrt(NoSignals)) * Q1 * (1 - (1 - 1/sqrt(NoSignals)) * Q1);
+% end
+% QAM_ExactPs
 
 
 BlockLength = 10240;
@@ -192,17 +199,17 @@ SimParam = struct(...
     'MaxRunTime', 300, ...    % Maximum simulation time in Seconds.
     'CheckPeriod', 50, ...    % Checking time in number of Trials.
     'MaxBitErrors', 10000*ones(size([-5:0.5:25])), ...
-    'MaxSymErrors', 10000*ones(size([-5:0.5:25])), ...
+    'MaxFrameErrors', 10000*ones(size([-5:0.5:25])), ...
     'minBER', 1e-6, ...
-    'minSER', 1e-6 );
+    'minFER', 1e-6 );
 
 Link = LinkSimulation(SimParam);
-Link.SingleSimulate();
+SimState = Link.SingleSimulate()
 
-semilogy(SNRdB, UpperSymbolBoundValue, 'r', SNRdB, QAM_ExactPs, '--k', SNRdB, Link.SimState.SER, '-.')
+% semilogy(SNRdB, UpperSymbolBoundValue, 'r', SNRdB, QAM_ExactPs, '--k', SNRdB, Link.SimState.SER, '-.')
 
 %% Testing InversePsUB and InversePbUB functions for 16-QAM:
-clear all
+clear classes
 NoSignals = 16;
 MM = QAM(NoSignals);
 SNRdB = -5:0.5:35;
@@ -264,7 +271,7 @@ SimParam = struct(...
     'minFER', 1e-5 );
 
 Link = LinkSimulation(SimParam);
-Link.SingleSimulate();
+SimState = Link.SingleSimulate()
 
 %% Testing TestLDPC_DVBS2 function as a UNIT TASK:
 clear classes
@@ -283,7 +290,7 @@ DVBS2Param = struct(...
 
 TaskUnit = struct(...
     'FunctionName', 'TestLDPC_DVBS2',...
-    'FunctionPath', 'C:\Users\Mohammad\Desktop\CMLProject\ACT\mat',...
+    'FunctionPath', 'C:\Users\Mohammad Fanaei\Desktop\Dropbox\CMLProject\ACT\PCS\projects\cml2\mat',...
     'SimulationParam', DVBS2Param,...
     'OutputFileName', 'QPSK_DVBS2.mat');
 
