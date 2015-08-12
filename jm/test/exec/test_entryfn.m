168215b4a4a83b380882b720e83d8c40567ec485
terry.ferrett <terry.ferrett@gmail.com>
2015-08-12 07:08:54 +0000

    reorganizing and committing job manager tests

--- jm/test/exec/test_entryfn.m
+++ jm/test/exec/test_entryfn.m
@@ -8,7 +8,7 @@ function out_struct = test_entryfn(in_struct)
 
 nnums = in_struct.nnums;   % read the number of variates from the input structure
 
-pause(300);                  % sleep for 30 seconds
+pause(15);                  % sleep for 30 seconds
 
 out_struct.nums = rand(1,nnums);  % write the variates to the output structure
 
